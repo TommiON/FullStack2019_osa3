63f083d6d350e1a4cdd446dc6d9f36378461894b
TommiON <44175014+TommiON@users.noreply.github.com>
2019-05-16 17:06:10 +0300

    Full Stack osa 3, yritys siirtää myös frontend Herokuun

---
+++ build/precache-manifest.bab3ea5ceb42ca50ba7215cc310f4c00.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ca2fd629691943873f2",
+    "url": "/static/js/main.c57edc42.chunk.js"
+  },
+  {
+    "revision": "b75963b72d437ee78378",
+    "url": "/static/js/2.5b8b90e2.chunk.js"
+  },
+  {
+    "revision": "f6484d3cb67689e797311309d963709f",
+    "url": "/index.html"
+  }
+];
--- puhelinluettelo_frontend/src/services/dbService.js
+++ puhelinluettelo_frontend/src/services/dbService.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
 
 const getAll = () => {
-    const request = axios.get('http://localhost:3001/api/persons')
+    const request = axios.get('/api/persons')
     return request.then(response => response.data)
 }
 
 const getOne = personId => {
-    const url = `http://localhost:3001/api/persons/${personId}`
+    const url = `/api/persons/${personId}`
     const request = axios.get(url)
     return request.then(response => response.data)
 }
 
 const add = newPerson => {
-    const request = axios.post('http://localhost:3001/api/persons', newPerson)
+    const request = axios.post('/api/persons', newPerson)
     return request.then(response => response.data)
 }
 
 const remove = personId => {
-    const url = `http://localhost:3001/api/persons/${personId}`
+    const url = `/api/persons/${personId}`
     const request = axios.delete(url)
     return request.then(response => response.data)
 }
 
 const replace = (personId, nimi, numero) => {
-    const url = `http://localhost:3001/api/persons/${personId}`
+    const url = `/api/persons/${personId}`
     console.log('nyt ollaan Axios-funktiossa')
     console.log('id: ', personId, " nimi: ", nimi, " uusi numero: ", numero)
     const request = axios.put(url, 
