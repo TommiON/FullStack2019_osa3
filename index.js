e8ffa4121c2b59c473134cd59476eeb43c8f8a8d
TommiON <44175014+TommiON@users.noreply.github.com>
2019-05-16 15:56:34 +0300

    Full Stack, osa 3, välicommit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+const cors = require('cors')
+app.use(cors())
+app.use(express.static('build'))
 
 // Huom tarkastajalle: Tiny-konfiguraatiota ei voinut käyttää, Morgan valitti että se on
 // "deprecated" ja jäädytti koko sovelluksen.
